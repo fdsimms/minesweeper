73de13cff57dba47f85e9b5cff1fe3b3b0eea8b7
STEVEN_FRANCIS <STEVEN.FRANCIS@appacademy.io>
2015-12-04 11:25:25 -0500

    Add to Board#initialize and create Board#place_mines.

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -1,9 +1,11 @@
 class Board
 
-  attr_accessor :grid
+  attr_accessor :grid, :mine_count
 
-  def initialize(grid = Array.new(9) {Array.new(9)})
+  def initialize(grid = Array.new(9) { Array.new(9) })
     @grid = grid
+    @mine_count = 0
+    place_mines
   end
 
   def [](pos)
@@ -14,6 +16,18 @@ class Board
   def []=(pos, value)
     x, y = pos
     grid[x][y] = value
+  end
+
+  def place_mines
+    until mine_count == 10
+      x, y = rand(0..8), rand(0..8)
+      board[x, y] = :b
+      mine_count += 1
+    end
+  end
+
+  def mine_count
+
   end
 
 end
